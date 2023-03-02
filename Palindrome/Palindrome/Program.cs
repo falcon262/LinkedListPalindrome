8dff15aea61326ed1598a11e59e0e0cdf9cc4411
falcon262 <67340827+falcon262@users.noreply.github.com>
2023-03-02 18:05:04 +0000

    Initial commit

---
+++ Palindrome/Palindrome/Program.cs
@@ -0,0 +1,62 @@
+﻿namespace Palindrome
+{
+
+    //Definition for singly-linked list.
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(IsPalindrome(new ListNode(1, new ListNode(2, new ListNode(2, new ListNode(1))))));
+        }
+
+        static List<int> list2 = new List<int>();
+
+        static ListNode PrintOut(ListNode list)
+        {
+            if (list.next != null)
+            {
+                list2.Add(list.val);
+                return PrintOut(list.next);
+            }
+            else
+            {
+                list2.Add(list.val);
+                return list;
+            }
+
+        }
+        static bool IsPalindrome(ListNode head)
+        {
+            PrintOut(head);
+            List<int> list = new List<int>();
+
+            foreach (var item in list2)
+            {
+                list.Add(item);
+            }
+
+            list2.Reverse();
+
+            if (list.SequenceEqual(list2))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
